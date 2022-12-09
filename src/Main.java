48603adadd0b3ca5b8c6211081fd2245d9fd5b27
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-09 13:34:14 +0300

    String-Length_Lesson_(Строки)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Строки-Length!");
+
+        //Задание 1
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Задание 2
+        System.out.println("Задание 2");
+
     }
 }
